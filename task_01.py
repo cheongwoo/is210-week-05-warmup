3fb5d03480fabad914e4af6f48896f6df6507305
Cheongwoo Kim <cheong_woo.kim@spsmail.cuny.edu>
2015-02-27 05:07:56 +0000

    Adding task 01~05

--- task_01.py
+++ task_01.py
@@ -4,6 +4,21 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Responses to a question by a motion and nudge certain number of times
+
+    Args:
+        wink (str): First motion response to question 'Know what I mean?'
+        numwink (int): Number of times Arg motion and nudge to be repeated
+
+    Returns:
+        str: 'Know what I mean?' followed by Arg wink and 'nudge ' set number
+             of times
+
+    Example:
+
+        >>> know_what_i_mean(wink='blink ', numwink=3)
+            Know what I mean? blink blink blink, nudge nudge nudge
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
